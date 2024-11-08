9a977c0463ae0abc11acb6ab3814e42544547cde
manojkapacia <manojkapasia721@gmail.com>
2024-11-08 09:41:23 +0530

    initial comment

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import OtpComponent from './components/auth/login-otp';
 import SearchComponent from './components/common/search';
 
 function App() {
+  const MESSAGES = {
+  searchHeading : "Searching For Your Documents",
+  seachSubHeading : "Check if your PF is at risk of getting stuck"
+}
   return (
+
     <Router>
       <Routes>
         <Route path="/" element={<LoginComponent />} />
         <Route path="/otpAssessment" element={<OtpComponent />} />
-        <Route path="/search" element={<SearchComponent />} />
+        <Route path="/search" element={<SearchComponent MESSAGES={MESSAGES}/>} />
         <Route path="*" element={<h2>404 - Page Not Found</h2>} />
       </Routes>
     </Router>
--- src/components/common/search.js
+++ src/components/common/search.js
@@ -1,16 +1,14 @@
 import '../../App.css';
 import serchImage from '../../images/serchImage.png';
 
-function SearchComponent() {
+function SearchComponent(props) {
     return (
         <div className="container">
             <div className="row">
                 <div className="d-flex flex-column  justify-content-center align-items-center vh-100">
                     <img src={serchImage} alt="SerchImage" height="150rem" width='150rem' />
-                    <div className="pfRiskheading">Searching For Your Documents</div>
-                    <div className='pfRiskSubHeading'>
-                        Check if your PF is at risk of getting stuck
-                    </div>
+                    <div className="pfRiskheading">{props.MESSAGES.searchHeading}</div>
+                    <div className='pfRiskSubHeading'>{props.MESSAGES.seachSubHeading}</div>
                 </div>
             </div>
         </div>
